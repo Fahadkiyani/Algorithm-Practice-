5c2053f63824534b346e4822dc2ec8ac836c8b2c
Fahad kiyani <fahadkiyani08@gmail.com>
2021-10-13 19:01:14 +0500

    countTinyPair algorithm finds the tiny pair of sum of two integer array and returns the result.

---
+++ countTinyPairs.js
@@ -0,0 +1,20 @@
+
+let a = [16, 1, 4, 2, 14]
+let b = [7, 11, 2, 0, 15]
+let k = 743;
+
+function countTinyPairs(a, b, k) {
+    let tinypairs = 0;
+    for (let i = 0; i < a.length; i++) {
+        let A = a[i].toString();
+        let B = b[(b.length - 1) - i].toString();
+        let concatinate = (A + B);
+        if (concatinate < k) {
+            tinypairs++
+        }
+
+    }
+    console.log(tinypairs);
+}
+
+countTinyPairs(a, b, k)
